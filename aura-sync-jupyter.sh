e38ba1194932423a4ae879b8205dbc9be5071b08
Dusan Klinec <dusan.klinec@gmail.com>
2019-05-28 14:20:11 +0200

    shell fix

--- aura-sync-jupyter.sh
+++ aura-sync-jupyter.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 SDIR="$(SHELL_SESSION_FILE='' && cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-rsync -av --progress aura:~/RTT/proc2.ipynb ${SDIR}/rtt_tools/
+rsync -av --progress aura:~/RTT/proc2.ipynb ${SDIR}
